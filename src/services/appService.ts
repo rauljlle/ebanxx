9d8f88ab71a8d0d2569464b2a791f7cf86a64642
rauljlle <rauljlle@gmail.com>
2024-07-22 23:02:19 -0300

    changes postReset call and adds DB instance to appService

--- src/services/appService.ts
+++ src/services/appService.ts
@@ -1,27 +1,17 @@
-import axios from 'axios';
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { DB } from '../utils/mockDB';
 
 class AppService {
+
+    #db = DB.instance;
+
     getRoot(): string {
         return 'Hire me ;)';
     }
 
-    async postReset(res: Response): Promise<void> {
-        try {
-            const apiUrl = process.env.API_URL;
-            if (!apiUrl) {
-                throw new Error('define API_URL in .env');
-            }
-
-            const apiResponse = await axios.post(`${apiUrl}/reset`);
-            res.status(apiResponse.status).send(apiResponse.data);
-        } catch (error) {
-            if (axios.isAxiosError(error) && error.response) {
-                res.status(error.response.status).send(error.response.data);
-            } else {
-                res.status(500).send('Internal Server Error');
-            }
-        }
+    postReset(res: Response): void {
+        this.#db.reset();
+        res.status(200);
     }
 }
 
