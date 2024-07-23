07ba923f28966058897d2b08f4d638c00886120f
rauljlle <rauljlle@gmail.com>
2024-07-22 23:54:29 -0300

    makes account moviments public and remos unncessary methods

--- src/account/entity.ts
+++ src/account/entity.ts
@@ -4,35 +4,20 @@ export class Account {
 
     #id: string;
     #balance: number;
-    static #db = DB.instance;
+    #db = DB.instance;
 
     constructor(id: string, balance: number){
         this.#id = id;
         this.#balance = balance;
-        Account.#db.insert(this);
+        this.#db.insert(this);
     }
 
-    public static getById(id: string): Account | undefined {
-        return Account.#db.findById(id);
-    }
-
-    private addBalance(amount: number){
+    addBalance(amount: number){
         this.#balance += amount;
     }
 
-    private removeBalance(amount: number){
+    removeBalance(amount: number){
         this.#balance -= amount;
-    }
-
-    private transferBalance(destinationId: string, amount:number){
-        let destination = Account.getById(destinationId);
-
-        // If it doesnt exist, creates it with an amount of zero
-        // so the rest of the code doesn't have to change
-        if (!destination) destination = new Account(destinationId, 0);
-
-        this.addBalance(amount);
-        destination.removeBalance(amount);
     }
 
     getId(): string {
